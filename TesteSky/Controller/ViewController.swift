b795207bb85b4247a5858b01be1570a9361a8191
Julio Cezar Souza <julio.souza@gmail.com>
2020-06-24 18:08:49 -0300

    initial project

---
+++ TesteSky/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TesteSky
+//
+//  Created by Julio Cezar de Souza on 22/06/20.
+//  Copyright © 2020 Julio Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ TesteSky/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  TesteSky
+//
+//  Created by Julio Cezar de Souza on 22/06/20.
+//  Copyright © 2020 Julio Souza. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let baseURL:String = "https://sky-exercise.herokuapp.com/api/Movies"
+    
+}
